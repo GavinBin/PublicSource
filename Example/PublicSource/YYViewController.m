95cb155b562cfd89709d1c25c8f30697f8065ec4
王兵 <klwanshua@163.com>
2017-06-23 12:57:19 +0800

    Initial commit

---
+++ Example/PublicSource/YYViewController.m
@@ -0,0 +1,29 @@
+//
+//  YYViewController.m
+//  PublicSource
+//
+//  Created by klwanshua@163.com on 06/23/2017.
+//  Copyright (c) 2017 klwanshua@163.com. All rights reserved.
+//
+
+#import "YYViewController.h"
+
+@interface YYViewController ()
+
+@end
+
+@implementation YYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
